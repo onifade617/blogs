6d1ceeb509a45c81240aeca60f9b2204873385f3
Onifade Monsuru <onifade617@gmail.com>
2025-03-13 10:39:36 +0100

    Canonical url, Day 1

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 from .models import Post
 from django.http import Http404
@@ -5,18 +6,25 @@ from django.http import Http404
 # Create your views here.
 def post_list(request):
     posts = Post.published.all()
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(
         request,
         'blog/post/list.html',
         {'posts': posts}
     )
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
-        id=id,
-        status=Post.Status.PUBLISHED
-    )
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
+        )
+    
     return render(
         request,
         'blog/post/detail.html',
