12e01fe84494b079a819da8441c7a0a40e792d80
Onifade Monsuru <onifade617@gmail.com>
2025-03-19 09:50:33 +0100

    blog post

--- blog/views.py
+++ blog/views.py
@@ -46,7 +46,7 @@ def post_detail(request, year, month, day, post):
     )
 
 def post_share(request, post_id):
-# Retrieve post by id
+    # Retrieve post by id
     post = get_object_or_404(
         Post,
         id=post_id,
@@ -55,10 +55,10 @@ def post_share(request, post_id):
     sent = False
 
     if request.method == 'POST':
-    # Form was submitted
+        # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
-# Form fields passed validation
+            # Form fields passed validation
             cd = form.cleaned_data
             post_url = request.build_absolute_uri(
                 post.get_absolute_url()
@@ -69,7 +69,7 @@ def post_share(request, post_id):
             )
             message = (
                 f"Read {post.title} at {post_url}\n\n"
-                f"{cd['name']}\'s comments: {cd['comments']}"
+                f"{cd['name']}'s comments: {cd['comments']}"
             )
             send_mail(
                 subject=subject,
@@ -78,15 +78,15 @@ def post_share(request, post_id):
                 recipient_list=[cd['to']]
             )
             sent = True
-# ... send email
-        else:
-            form = EmailPostForm()
-        return render(
-            request,
-            'blog/post/share.html',
-            {
-                'post': post,
-                'form': form
-            }
-        )
-    
+    else:
+        form = EmailPostForm()
+
+    return render(
+        request,
+        'blog/post/share.html',
+        {
+            'post': post,
+            'form': form,
+            'sent': sent
+        }
+    )
